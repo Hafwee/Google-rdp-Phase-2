fcaf973228d14f634a5e95682bd2922dea888a01
Hafwee <86737197+Hafwee@users.noreply.github.com>
2021-09-10 12:49:36 -0400

    Update GRDP-VPS.ps1

--- GRDP-VPS.ps1
+++ GRDP-VPS.ps1
@@ -12,21 +12,20 @@ New-Item -Path "C:\Users\Public\Desktop\Teohook Setup" -Name "VPN" -ItemType Dir
 New-Item -Path "C:\Users\Public\Desktop\Teohook Setup" -Name "Latest GT" -ItemType Directory
 New-Item -Path "C:\Users\Public\Desktop\Teohook Setup" -Name "Important" -ItemType Directory
 New-Item -Path "C:\Users\Public\Desktop\Teohook Setup" -Name "DLL and Injector" -ItemType Directory
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/teohook.dll -OutFile "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\teohook.dll"
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/Extreme-Injector.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\Extreme-Injector.exe"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/TeoHook.dll -OutFile "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\teohook.dll"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/Injector.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\DLL and Injector\Extreme-Injector.exe"
 Invoke-WebRequest https://hide.me/downloads/Hide.me-Setup-3.8.3.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\VPN\Hide.me-Setup.exe"
-Invoke-WebRequest https://ubistatic-a.akamaihd.net/0098/31789/GrowtopiaInstaller.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\Latest GT\GrowtopiaInstaller.exe"
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/items.dat -OutFile "C:\Users\Public\Desktop\Teohook Setup\Important\items.dat"
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/crashfix.txt -OutFile "C:\Users\Public\Desktop\Teohook Setup\Important\crashfix.txt"
-Invoke-WebRequest https://www.dl.dropboxusercontent.com/s/5l2mfoffvr8m7at/Growtopia.zip?dl=0 -OutFile "C:\Users\Public\Desktop\Growtopia.zip"
+Invoke-WebRequest https://ubistatic-a.akamaihd.net/0098/39940/GrowtopiaInstaller.exe -OutFile "C:\Users\Public\Desktop\Teohook Setup\Latest GT\GrowtopiaInstaller.exe"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/items.dat -OutFile "C:\Users\Public\Desktop\Teohook Setup\Important\items.dat"
+Invoke-WebRequest https://dl.dropboxusercontent.com/s/0nqblnbzmthhwcw/Growtopia.rar?dl=0 -OutFile "C:\Users\Public\Desktop\Growtopia.zip"
 Invoke-WebRequest https://www.dl.dropboxusercontent.com/s/79kacg0wdlbrjgt/NordVPN-safon4e.exe?dl=0 -OutFile "C:\Users\Public\Desktop\NordVPN-Setup.exe"
-Invoke-WebRequest https://raw.githubusercontent.com/OnurCreed/secretsource/main/Files/Programs/nordvpn-acc.txt -OutFile "C:\Users\Public\Desktop\nord-acc.txt"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/nordvpn-acc.txt -OutFile "C:\Users\Public\Desktop\nord-acc.txt"
 Expand-Archive -Path C:\Users\Public\Desktop\Growtopia.zip -DestinationPath C:\Users\runneradmin\Appdata\Local
 Expand-Archive -Path C:\Users\Public\Desktop\Growtopia.zip -DestinationPath C:\Users\Public\Desktop\FixedGT1
 Expand-Archive -Path C:\Users\Public\Desktop\Growtopia.zip -DestinationPath C:\Users\Public\Desktop\FixedGT2
 New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "Normal Growtopia.lnk" -Value "C:\Users\runneradmin\Appdata\Local\Growtopia\Growtopia.exe"
-Invoke-WebRequest https://github.com/chichiyacompany/Growtopia/raw/master/Growtopia.exe -OutFile "C:\Users\Public\Desktop\FixedGT1\Growtopia\FixedGT1.exe"
-Invoke-WebRequest https://github.com/chichiyacompany/Growtopia/raw/master/Growtopia.exe -OutFile "C:\Users\Public\Desktop\FixedGT2\Growtopia\FixedGT2.exe"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/Growtopia1.exe -OutFile "C:\Users\Public\Desktop\FixedGT1\Growtopia\FixedGT1.exe"
+Invoke-WebRequest https://github.com/Hafwee/Growtopia/releases/download/1/Growtopia1.exe -OutFile "C:\Users\Public\Desktop\FixedGT2\Growtopia\FixedGT2.exe"
 New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "Fixed GT1.lnk" -Value "C:\Users\Public\Desktop\FixedGT1\Growtopia\FixedGT1.exe"
 New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "Fixed GT2.lnk" -Value "C:\Users\Public\Desktop\FixedGT2\Growtopia\FixedGT2.exe"
 Remove-Item C:\Users\Public\Desktop\FixedGT1\Growtopia\Growtopia.exe -Recurse
